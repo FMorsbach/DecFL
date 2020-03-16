13f22636b48cb0690f0460da53ae1a8c6c1b84f2
FMorsbach <felixmorsbach@web.de>
2020-03-16 20:43:28 +0100

    Added system control operations.

---
+++ control/control.go
@@ -0,0 +1,65 @@
+package control
+
+import (
+	"log"
+	"time"
+
+	"github.com/FMorsbach/DecFL/communcation/chain"
+	"github.com/FMorsbach/DecFL/communcation/storage"
+	"github.com/FMorsbach/DecFL/models/MNIST"
+	"github.com/FMorsbach/DecFL/training/tensorflow"
+)
+
+func Initialize() {
+	config, weights := MNIST.GenerateInitialModel()
+
+	configAddress, weightsAddress := storage.StoreInitialModel(config, weights)
+	chain.DeployInitialModel(configAddress, weightsAddress)
+}
+
+func Iterate() {
+	// load the storage addresses from the chain
+	configAddress := chain.ModelConfigurationAddress()
+	weightsAddress := chain.GlobalWeightsAddress()
+
+	// load the model from the storage
+	config, err := storage.LoadGlobalState(configAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	weights, err := storage.LoadGlobalState(weightsAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// train locally
+	localUpdate := tensorflow.Run(config, weights)
+
+	// write the update to the storage
+	updateAddress, err := storage.StoreUpdates(localUpdate)
+
+	// write the address of the stored update to the chain
+	chain.AppendUpdateAddress(string(time.Now().Unix()), updateAddress)
+
+}
+
+func Aggregate() {
+	// load the local udpate addresses from the chain
+	updateAddresses := chain.LocalUpdateAddresses()
+
+	// load the local updates from storage
+	updates := storage.LocalUpdates(updateAddresses)
+
+	// aggregate the local updates
+	globalWeights := tensorflow.Aggregate(updates)
+
+	// write the new global weights to storage
+	globalWeightsAddress, _ := storage.StoreUpdates(globalWeights)
+
+	// write the new global weights storage address to the chain
+	chain.UpdateGlobalWeightsAddress(globalWeightsAddress)
+
+	// empty the local update storage
+	chain.CleanLocalUpdateStore()
+}
