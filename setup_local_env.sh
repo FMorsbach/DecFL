54305e71bc54a43fa240c15d4ea9c6c8dbeeb5eb
FMorsbach <felixmorsbach@mailbox.org>
2020-11-09 12:04:24 +0100

    setup_local_env.sh will no longer throw an error if decfl_net already exists

--- setup_local_env.sh
+++ setup_local_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker network create decfl_net
+sudo docker network inspect decfl_net >/dev/null 2>&1 ||  sudo docker network create decfl_net
 sudo docker run --rm -d --name chain -p 127.0.0.1:8545:8545 --network decfl_net trufflesuite/ganache-cli -m "$(cat scenarios/local/mnemonic)"
 sudo docker run --rm -d --name redis -p 127.0.0.1:6379:6379 --network decfl_net redis:latest
