6bce08ba7c33e23056a1845b2f4651b1d192bc43
FMorsbach <felixmorsbach@mailbox.org>
2020-11-11 16:32:27 +0100

    Added scripts (tocsv + R) for bargraphs on different phases.

--- benchmarks/scripts/graphs.R
+++ benchmarks/scripts/graphs.R
@@ -1,7 +1,17 @@
 
 
 #import rounds.csv into data
-
 agg = aggregate(list("Time" = data$TIME), list("Model" = data$MODEL, "Clients" = data$CLIENTS, "Node ID" = data$NODE_ID, "Round" = data$ROUND), FUN=sum)
-
 boxplot(Time ~ Clients, data = agg)
+
+
+
+# import phases.csv into data
+data$CLIENTS <- as.factor(b$CLIENTS)
+data$STEP <- as.factor(b$STEP)
+data$MODEL <- as.factor(b$MODEL)
+data$NODE_ID <- as.factor(b$NODE_ID)
+ggplot(data, aes(fill=data$STEP, y=data$TIME, x=data$CLIENTS)) + 
+  geom_bar(position="fill", stat="identity") +
+  xlab("Clients") +
+  ylab("")
