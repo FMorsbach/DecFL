2796f4fc930e2e7c374a43f812ef4e32caf45e28
FMorsbach <felixmorsbach@mailbox.org>
2020-11-17 18:33:53 +0100

    Added script/playbook to retrieve docker logs from remote host. Fixed typo in R script.

--- benchmarks/scripts/graphs.R
+++ benchmarks/scripts/graphs.R
@@ -7,10 +7,11 @@ boxplot(Time ~ Clients, data = agg)
 
 
 # import phases.csv into data
-data$CLIENTS <- as.factor(b$CLIENTS)
-data$STEP <- as.factor(b$STEP)
-data$MODEL <- as.factor(b$MODEL)
-data$NODE_ID <- as.factor(b$NODE_ID)
+library(ggplot2)
+data$CLIENTS <- as.factor(data$CLIENTS)
+data$STEP <- as.factor(data$STEP)
+data$MODEL <- as.factor(data$MODEL)
+data$NODE_ID <- as.factor(data$NODE_ID)
 ggplot(data, aes(fill=data$STEP, y=data$TIME, x=data$CLIENTS)) + 
   geom_bar(position="fill", stat="identity") +
   xlab("Clients") +
